ca7952ed49eed5fb682967eb6f56eced28165440
Ami.Raian <ami.robirai@gmail.com>
2024-08-24 12:13:59 +0600

    Home page pudate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,6 @@ const HomePage = () => {
                             Title
                         </label>
                         <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="productTitle" type="text" placeholder="Jane" />
-                        <p className="text-red-500 text-xs italic">Please fill out this field.</p>
                     </div>
 
                     <div className="w-full md:w-1/2 px-3">
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -5,20 +5,6 @@ const Header = () => {
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </div>
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
@@ -26,16 +12,12 @@ const Header = () => {
                         <Link to="/allProduct"><a>All Product</a></Link>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     <a href=""><Link to="/" className="mr-3">Home</Link></a>
                     <a href=""><Link to="/allProduct">All Product</Link></a>
                 </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
             </div>
         </div>
     );
